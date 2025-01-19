85190b161e329f903235f29217e928e62e7e7cfe
Igor-Konyaga <konyaga.igor10@gmail.com>
2025-01-19 20:05:47 +0200

    initial project

---
+++ src/app/components/FAQ/Item/Item.tsx
@@ -0,0 +1,29 @@
+'use client';
+import { FC, useState } from 'react';
+import styles from './Item.module.scss';
+import { ArrowIcon } from '../../CustomIcons/ArrowIcon/ArrowIcon';
+
+type Props = {
+  title: string;
+  desc: string;
+};
+
+export const Item: FC<Props> = ({ title, desc }) => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  return (
+    <li className={styles.item}>
+      <button
+        onClick={() => setOpen(!open)}
+        className={styles.btn}
+        type="button"
+      >
+        <span className={styles.title}>{title}</span>
+        <ArrowIcon active={open} className={styles.icon} />
+      </button>
+      <p className={open ? `${styles.desc} ${styles.open}` : styles.desc}>
+        {desc}
+      </p>
+    </li>
+  );
+};
---
+++ src/app/components/Price/Price.tsx
@@ -0,0 +1,9 @@
+import styles from './Price.module.scss';
+
+export const Price = () => {
+  return (
+    <section id="price" className={styles.price}>
+      <h2 className={styles.priceTitle}>Прайс</h2>
+    </section>
+  );
+};
