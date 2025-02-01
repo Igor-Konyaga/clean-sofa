974ab50adf50c09786987ae04e2fc6bf59883c11
Igor-Konyaga <konyaga.igor10@gmail.com>
2025-02-01 22:26:43 +0200

    update key

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/clean-sofa',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
