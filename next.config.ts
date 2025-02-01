453d84c58431c1da9c538e4ad2b89aebff1e7427
Igor-Konyaga <konyaga.igor10@gmail.com>
2025-02-01 22:36:33 +0200

    test

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/clean-sofa',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
