2772517603e85ad99e76f3f08b7c48748d45ba4f
Igor-Konyaga <konyaga.igor10@gmail.com>
2025-02-11 09:20:30 +0200

    update

--- src/app/services/telegremMessage.ts
+++ src/app/services/telegremMessage.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const TOKEN = '';
-const CHAT_ID = '';
-const URL_API_TG = ``;
+const TOKEN = '7808532859:AAGmDwaKKsGoxpJC2thb2O6-czKNB8r42j8';
+const CHAT_ID = '-4784064551';
+const URL_API_TG = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
 
 export const fetchTelegramMessage = async (message: string) => {
   const data = await axios.post(URL_API_TG, {
